a9fdce812a967336e7663a421f1a5bf73fb3f042
Sue Tarazi <suemachtley@gmail.com>
2020-12-04 16:40:40 -0800

    cleaned up and working :)

--- testServer.go
+++ testServer.go
@@ -6,10 +6,22 @@ import (
 	"net/http"
 )
 
+func greetingHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "Method GET is not supported", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf(w, "Hello and happy Friday!")
+}
+
 func main() {
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello and Happy Friday!")
-	})
+	http.HandleFunc("/hello", greetingHandler)
 
 	fmt.Printf("Hey, lets start a server on port 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
